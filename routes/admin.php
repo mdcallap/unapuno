7c7c7cd2c9409e3d6506cc46ad97e287e4e1c8c8
mdcallap <mdcallap@gmail.com>
2024-01-11 11:17:24 -0500

    push 2

--- routes/admin.php
+++ routes/admin.php
@@ -18,8 +18,8 @@ use Illuminate\Support\Facades\Route;
 
 /* Route::get('/', function () {
     return view('welcome');
-}); */
-
+});
+ */
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
