45e8b97f3181dfab9877448fc331d2a0104b7049
mdcallap <mdcallap@gmail.com>
2025-04-14 10:39:23 -0500

    sad

--- app/Livewire/Web/Posts.php
+++ app/Livewire/Web/Posts.php
@@ -8,7 +8,7 @@ class Posts extends Component
 {
     public function render()
     {
-        $posts = \App\Models\Post::where('status','1')->latest('id');
+        $posts = \App\Models\Post::where('status','1')->latest('id')->get();
         return view('livewire.web.posts',compact('posts'))->layout('layouts.web');
     }
 }
