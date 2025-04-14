dd22cd7d2a184596ed79fe88a98d1b14e351a506
mdcallap <mdcallap@gmail.com>
2025-04-14 10:14:14 -0500

    Update Posts.php

--- app/Livewire/Web/Posts.php
+++ app/Livewire/Web/Posts.php
@@ -8,7 +8,7 @@ class Posts extends Component
 {
     public function render()
     {
-        $posts = \App\Models\Post::where('status','1')->get();
+        $posts = \App\Models\Post::where('status','1')->latest('id');
         return view('livewire.web.posts',compact('posts'))->layout('layouts.web');
     }
 }
