8c5801a5e8528cb2cf77197c64fd968517930a06
mdcallap <mdcallap@gmail.com>
2025-04-14 10:29:47 -0500

    Update Documents.php

--- app/Livewire/Web/Documents.php
+++ app/Livewire/Web/Documents.php
@@ -8,7 +8,7 @@ class Documents extends Component
 {
     public function render()
     {
-        $docs = \App\Models\Post::where('type','1')->where('status','1')->get();
+        $docs = \App\Models\Post::where('type','1')->where('status','1')->latest('id');
         return view('livewire.web.documents',compact('docs'))->layout('layouts.web');
     }
 }
